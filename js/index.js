a4f70490e97bfb1d1bc9b1116672911d31b9ef56
GianZhang-SXAU <zhangjianan545@gmail.com>
2024-09-06 11:21:19 +0800

    a

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
  * @Author: GianZhang-SXAU zhangjianan545@gmail.com
  * @Date: 2024-09-06 09:25:56
  * @LastEditors: GianZhang-SXAU zhangjianan545@gmail.com
- * @LastEditTime: 2024-09-06 10:14:14
+ * @LastEditTime: 2024-09-06 11:18:50
  * @FilePath: \无人车零售\js\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
